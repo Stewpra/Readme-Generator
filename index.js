6a1c6534bee390a6a12126a2d7d14cc4b04fbb58
Stewpra <wilbstewart7@gmail.com>
2024-03-26 19:58:23 -0400

    added questions array and prompt. Also some generate readme

--- index.js
+++ index.js
@@ -1,7 +1,74 @@
-// TODO: Include packages needed for this application
+// DEPENDENCIES
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// DATA
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter the title of your project:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Describe the Usage of the app:',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Describe how to contribute:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Describe any tests:',
+  },
+  {
+    type: 'input',
+    name: 'gitHub',
+    message: 'Add you github username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Add your email:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Pick your license',
+  },
+];
+
+inquirer.prompt(questions).then((answers) => {
+  const readmeContent = generateREADME(answers);
+  fs.writeFile('README.md', readmeContent, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README.md file generated successfully!');
+    }
+  });
+});
+
+// FUNCTIONS
+
+// USER INTERACTIONS
+
+// INITILIZATIONS
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
